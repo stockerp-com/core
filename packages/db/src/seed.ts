2da662ade1391fc76043b485a07f551332930890
artemchs <artemchs123@gmail.com>
2024-08-05 20:11:44 +0200

    feat: :card_file_box: set up db package

---
+++ packages/db/src/seed.ts
@@ -0,0 +1,10 @@
+(async () => {
+    try {
+        // TODO: Add seed data here
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    } finally {
+        await db?.$disconnect();
+    }
+})();
