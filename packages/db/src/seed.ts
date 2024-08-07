c09152bd1b72c5b6e65265b03988db4e0303ded7
artemchs <artemchs123@gmail.com>
2024-08-07 09:31:09 +0200

    feat(packages:db): :seedling: add a script to seed the db

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,12 +1,19 @@
 import { db } from './client.js';
+import { hash } from 'bcrypt';
 
 (async () => {
-    try {
-        // TODO: Add seed data here
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    } finally {
-        await db.$disconnect();
-    }
+  try {
+    await db.admin.create({
+      data: {
+        email: 'john@doe',
+        fullName: 'John Doe',
+        pwHash: await hash('password', 10),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  } finally {
+    await db.$disconnect();
+  }
 })();
