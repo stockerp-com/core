4983721e44b31eb446b7c483552475742e5ee943
artemchs <artemchs123@gmail.com>
2024-08-15 15:49:51 +0200

    feat(packages:trpc): :necktie: add functionality to find current organization

--- packages/trpc/src/admin-server/routers/organization/organization.router.ts
+++ packages/trpc/src/admin-server/routers/organization/organization.router.ts
@@ -1,6 +1,7 @@
 import { router } from '../../trpc.js';
 import { addHandler } from './handlers/add.handler.js';
 import { findAllInfiniteHandler } from './handlers/find-all.handler.js';
+import { findCurrentHandler } from './handlers/find-current.handler.js';
 import { findOneHandler } from './handlers/find-one.handler.js';
 import { useHandler } from './handlers/use.handler.js';
 
@@ -9,4 +10,5 @@ export const organizationRouter = router({
   add: addHandler,
   use: useHandler,
   findOne: findOneHandler,
+  findCurrent: findCurrentHandler,
 });
