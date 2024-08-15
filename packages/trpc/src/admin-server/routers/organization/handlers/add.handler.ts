400ab9cd4f1e5235aa90f9ccae745ddd2e03d92c
artemchs <artemchs123@gmail.com>
2024-08-15 15:04:00 +0200

    feat(packages:trpc): :necktie: add functionality to create an organization

--- packages/trpc/src/admin-server/routers/organization/handlers/add.handler.ts
+++ packages/trpc/src/admin-server/routers/organization/handlers/add.handler.ts
@@ -1,3 +1,19 @@
 import { authenticatedProcedure } from '../../../procedures/authenticated.js';
+import { addSchema } from '@retailify/validation/admin/organization/add.schema';
 
-export const addHandler = authenticatedProcedure;
+export const addHandler = authenticatedProcedure
+  .input(addSchema)
+  .mutation(async ({ ctx, input }) => {
+    return await ctx.db?.organization.create({
+      data: {
+        name: input.name,
+        description: input.description,
+        staff: {
+          create: {
+            employeeId: ctx.session!.id,
+            role: 'ORGANIZATION_ADMIN',
+          },
+        },
+      },
+    });
+  });
