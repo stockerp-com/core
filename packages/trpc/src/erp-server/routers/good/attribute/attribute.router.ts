97d77970598bd60bf687b55f8aa8191297fa8b2d
Artem Chernysh <artemchs123@gmail.com>
2024-09-05 12:12:27 +0200

    feat(packages:i18n): :globe_with_meridians: add english translations for attribute values

--- packages/trpc/src/erp-server/routers/good/attribute/attribute.router.ts
+++ packages/trpc/src/erp-server/routers/good/attribute/attribute.router.ts
@@ -1,8 +1,18 @@
 import { router } from '../../../trpc.js';
+import { addHandler } from './handlers/add.js';
+import { deleteHandler } from './handlers/delete.js';
+import { editHandler } from './handlers/edit.js';
+import { findManyInfiniteHandler } from './handlers/find-many.js';
+import { findOneHandler } from './handlers/find-one.js';
 import { valueRouter } from './value/value.router.js';
 
 export const attributeRouter = router({
   // Handlers:
+  add: addHandler,
+  edit: editHandler,
+  findOne: findOneHandler,
+  findManyInfinite: findManyInfiniteHandler,
+  delete: deleteHandler,
   // Routers:
   value: valueRouter,
 });
